c589030644247d1463fd10494b660a8c911ca32e
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-25 21:13:58 +0500

    AdminPages Created

---
+++ src/Eleshop.Desktop/Pages/Admins/PhonePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Eleshop.Desktop.Components.Admins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eleshop.Desktop.Pages.Admins
+{
+    /// <summary>
+    /// Interaction logic for PhonePage.xaml
+    /// </summary>
+    public partial class PhonePage : Page
+    {
+        public PhonePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPhoneCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            PhoneViewComponent phone = new PhoneViewComponent();
+            MainWP.Children.Add(phone);
+            PhoneViewComponent phone2 = new PhoneViewComponent();
+            MainWP.Children.Add(phone2);
+            PhoneViewComponent phone3 = new PhoneViewComponent();
+            MainWP.Children.Add(phone3);
+        }
+    }
+}
---
+++ src/Eleshop.Desktop/Pages/Admins/ProductPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Eleshop.Desktop.Components.Admins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eleshop.Desktop.Pages.Admins
+{
+    /// <summary>
+    /// Interaction logic for ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProductViewComponent product = new ProductViewComponent();
+            MainWP.Children.Add(product);
+            ProductViewComponent product1 = new ProductViewComponent();
+            MainWP.Children.Add(product1);
+            ProductViewComponent product2 = new ProductViewComponent();
+            MainWP.Children.Add(product2);
+            ProductViewComponent product3 = new ProductViewComponent();
+            MainWP.Children.Add(product3);
+            ProductViewComponent product4 = new ProductViewComponent();
+            MainWP.Children.Add(product4);
+        }
+    }
+}

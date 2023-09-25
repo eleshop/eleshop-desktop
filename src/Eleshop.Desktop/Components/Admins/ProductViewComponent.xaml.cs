454312d930901d1d0cf77745fbdf4de08cc761f1
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-25 21:13:11 +0500

    AdminComponents Created

---
+++ src/Eleshop.Desktop/Components/Admins/ProductViewComponent.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eleshop.Desktop.Components.Admins
+{
+    /// <summary>
+    /// Interaction logic for ProductViewComponent.xaml
+    /// </summary>
+    public partial class ProductViewComponent : UserControl
+    {
+        public ProductViewComponent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadMore_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
